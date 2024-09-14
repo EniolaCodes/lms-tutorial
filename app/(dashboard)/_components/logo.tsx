8484468e1e86a400573e5a2e0a2416cdd8f5cfa1
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-09-14 21:13:41 +0100

    feat: added layouts, sidebar and navbar

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const Logo = () => {
+    return (
+        <Image
+        width={50}
+        height={50}
+        alt="logo"
+        src="/lmsLogo.jpg"
+        />
+    )
+}
